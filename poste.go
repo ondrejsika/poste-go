ae22891d7710f722e791267d43138b46eba05bc3
Ondrej Sika <ondrej@ondrejsika.com>
2020-05-19 20:05:08 +0200

    Replace API user to username

--- poste.go
+++ poste.go
@@ -6,26 +6,26 @@ import (
 
 type PosteAPI struct {
 	origin   string
-	user     string
+	username string
 	password string
 }
 
-func New(origin string, user string, password string) PosteAPI {
-	api := PosteAPI{origin, user, password}
+func New(origin string, username string, password string) PosteAPI {
+	api := PosteAPI{origin, username, password}
 	return api
 }
 
 func (api PosteAPI) get(url string, query map[string]string) (*resty.Response, error) {
 	client := resty.New()
-	return client.R().SetBasicAuth(api.user, api.password).SetQueryParams(query).Get(api.origin + url)
+	return client.R().SetBasicAuth(api.username, api.password).SetQueryParams(query).Get(api.origin + url)
 }
 
 func (api PosteAPI) post(url string, body map[string]interface{}) (*resty.Response, error) {
 	client := resty.New()
-	return client.R().SetBasicAuth(api.user, api.password).SetBody(body).Post(api.origin + url)
+	return client.R().SetBasicAuth(api.username, api.password).SetBody(body).Post(api.origin + url)
 }
 
 func (api PosteAPI) delete(url string) (*resty.Response, error) {
 	client := resty.New()
-	return client.R().SetBasicAuth(api.user, api.password).Delete(api.origin + url)
+	return client.R().SetBasicAuth(api.username, api.password).Delete(api.origin + url)
 }
